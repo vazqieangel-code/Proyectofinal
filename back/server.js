095ed610410e4270dfdd52499f71f873d50cce66
AngelVaz12 <frijolesfritos18@gmail.com>
2026-06-16 00:56:12 -0600

    Prueba version deploy

--- back/server.js
+++ back/server.js
@@ -4,6 +4,8 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
+console.log("DEBUG VERSION FCE9643");
+
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
